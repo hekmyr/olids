a168e1feae7278c678f3c14b19721895e444d9be
hekma <contact@hekma.dev>
2025-03-09 10:44:30 +0100

    [webapi] Updated security config

--- webapi/src/main/java/dev/hekmyr/olids/api/config/SecurityConfig.java
+++ webapi/src/main/java/dev/hekmyr/olids/api/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package dev.hekmyr.olids.api.config;
 
 import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,13 +15,16 @@ import org.springframework.web.cors.CorsConfiguration;
 @Configuration
 public class SecurityConfig {
 
+  @Value("${cors.allowed-origins}")
+  private List<String> allowedOrigins;
+
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http.httpBasic(Customizer.withDefaults());
     http.cors(cors ->
       cors.configurationSource(request -> {
         var config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedMethods(
           Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")
         );

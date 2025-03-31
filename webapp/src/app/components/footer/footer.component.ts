50dba2375c86cb176983490c72945cb87c703fd4
hekma <contact@hekma.dev>
2025-03-31 05:06:05 +0200

    [webapp] Update footer component and improve breakpoints

--- webapp/src/app/components/footer/footer.component.ts
+++ webapp/src/app/components/footer/footer.component.ts
@@ -7,21 +7,22 @@ import { APP_TITLE } from '../../constant';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <footer class="bg-alternative px-page py-8">
+    <footer class="bg-alternative px-page sm:px-page md:px-page lg:px-page xl:px-page py-3 sm:py-6 lg:py-12">
       <div class="flex justify-between items-start">
-        <div class="text-[96px] font-heading">{{ appTitle }}</div>
-        <div>
+        <a href="/" class="text-[24px] sm:text-[48px] md:text-[64px] lg:text-[80px] xl:text-[96px] font-heading">{{ appTitle }}</a>
+        <!-- <div>
           <ul class="flex flex-col gap-2">
-            <li><a href="#" class="text-footer-link">Link 1</a></li>
-            <li><a href="#" class="text-footer-link">Link 2</a></li>
-            <li><a href="#" class="text-footer-link">Link 3</a></li>
-          </ul>
-        </div>
-        <div class="ml-32">
-          <ul class="flex flex-col gap-2">
-            <li><a href="#" class="text-footer-link">Link 4</a></li>
-            <li><a href="#" class="text-footer-link">Link 5</a></li>
+            <li><a href="#" class="text-footer-link">Profile</a></li>
+            <li><a href="#" class="text-footer-link">Réservations</a></li>
             <li><a href="#" class="text-footer-link">Link 6</a></li>
+          </ul>
+        </div> -->
+        <div>
+        <!-- class="ml-32" -->
+          <ul class="flex flex-col gap-2">
+            <li><a href="#" class="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-averia">À propos</a></li>
+            <li><a href="#" class="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-averia">Contactez-nous</a></li>
+            <li><a href="#" class="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-averia">FAQ</a></li>
           </ul>
         </div>
       </div>

2e7f6d2b1da349f71cc80ac78258aa29ede08433
hekma <contact@hekma.dev>
2025-03-31 10:22:01 +0200

    [webapp] Update h2 component

--- webapp/src/app/components/typography/h2.component.ts
+++ webapp/src/app/components/typography/h2.component.ts
@@ -3,7 +3,7 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-h2',
   standalone: true,
-  template: `<h2 class="text-h2 font-bold {{ className() }}">
+  template: `<h2 class="text-h2 sm:text-h2 md:text-h2 lg:text-h2 xl:text-h2 font-bold font-averia {{ className() }}">
     <ng-content></ng-content>
   </h2>`
 })

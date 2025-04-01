2bfb62292ec77f3a66eca3fa395325a64fbfbaac
hekma <contact@hekma.dev>
2025-04-01 15:08:47 +0200

    [webapp] Contact page responsive

--- webapp/src/app/pages/contact/contact.page.ts
+++ webapp/src/app/pages/contact/contact.page.ts
@@ -18,45 +18,45 @@ import { ThreeDButtonComponent } from '../../components/three-d-button/three-d-b
     ThreeDButtonComponent
   ],
   template: `
-    <div class="min-h-screen">
-      <app-menubar></app-menubar>
-      <div class="px-page pt-page pb-page flex gap-16">
-        <div class="flex flex-col gap-8 min-w-[480px]">
-          <div class="flex flex-col gap-4">
-            <app-h3>Coordonnées</app-h3>
-            <div class="flex flex-col gap-3">
-              <div class="flex justify-between">
-                <span class="font-semibold">Adresse</span>
-                <span>Hadikgasse 96<br />1140 Wien</span>
-              </div>
-              <div class="flex justify-between">
-                <span class="font-semibold">Email</span>
-                <span>misael.mcdermott&#64;monsejour.at</span>
-              </div>
-              <div class="flex justify-between">
-                <span class="font-semibold">Numéro de téléphone</span>
-                <span>+(43) (01) 8121287</span>
-              </div>
-            </div>
+  <div class="">
+    <app-menubar></app-menubar>
+    <div class="gap-8 px-page sm:px-page md:px-page lg:px-page xl:px-page pt-8 lg:pt-16">
+      <div class="mx-auto max-w-[580px] lg:mx-0 lg:w-[390px]">
+      <div class="flex flex-col gap-4">
+        <app-h3>Coordonnées</app-h3>
+        <div class="flex flex-col gap-3">
+          <div class="flex justify-between">
+            <span class="font-semibold">Adresse</span>
+            <span>Hadikgasse 96<br />1140 Wien</span>
           </div>
-          <div class="flex flex-col gap-4">
-            <app-h3>Nous contacter</app-h3>
-            <form class="flex flex-col gap-6">
-              <div class="flex flex-col gap-3">
-                <app-custom-input placeholder="Email"> </app-custom-input>
-                <app-custom-input placeholder="Subject"> </app-custom-input>
-              </div>
-              <app-custom-textarea placeholder="Message"> </app-custom-textarea>
-              <app-three-d-button>Submit</app-three-d-button>
-            </form>
+          <div class="flex justify-between">
+            <span class="font-semibold">Email</span>
+            <span>misael.mcdermott&#64;monsejour.at</span>
+          </div>
+          <div class="flex justify-between">
+            <span class="font-semibold">Numéro de téléphone</span>
+            <span>+(43) (01) 8121287</span>
           </div>
         </div>
-        <img
-          src="images/contact_page_side_img.jpg"
-          alt="Contact Image"
-          class="flex h-[calc(100vh-228px)] w-full object-cover rounded-md" />
       </div>
-    </div>
-  `
+      <div class="flex flex-col gap-4">
+        <app-h3>Nous contacter</app-h3>
+        <form class="flex flex-col gap-6">
+        <div class="flex flex-col gap-3">
+          <app-custom-input placeholder="Email"> </app-custom-input>
+          <app-custom-input placeholder="Subject"> </app-custom-input>
+        </div>
+        <app-custom-textarea placeholder="Message"> </app-custom-textarea>
+        <app-three-d-button>Submit</app-three-d-button>
+        </form>
+      </div>
+      </div>
+    </div> 
+    <img
+      src="images/contact_page_side_img.jpg"
+      alt="Contact Image"
+      class="w-full object-cover hidden lg:block lg:absolute lg:top-[calc(112px+64px)] lg:w-[calc(100vw-64px-64px-390px-64px)] lg:left-[calc(390px+64px+64px)] lg:h-[calc(100vh-112px-64px-64px)] lg:rounded-[8px]"/>
+  </div>
+   `
 })
 export class ContactPage {}

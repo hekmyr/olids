cc459e9b71d9e16c878ec8245c0370275a3ef065
hekma <contact@hekma.dev>
2025-06-01 17:35:12 +0200

    [api] Delete DbService
    DbService was replace by repositories as it shoud

--- api/src/main/java/dev/hekmyr/holidays/api/service/UserService.java
+++ api/src/main/java/dev/hekmyr/holidays/api/service/UserService.java
@@ -47,43 +47,4 @@ public class UserService {
     public UUID getAuthenticatedUserId() {
         return userRepository.findIdByEmail(getAuthenticatedUsername());
     }
-
-    public static UserDTO updateUser(String username, UserUpdateDTO dto) {
-        var session = DbService.buildSessionFactory().openSession();
-        var tx = session.getTransaction();
-        try {
-            tx.begin();
-            var hql =
-                "UPDATE User u SET " +
-                "u.lastName = :lastName, " +
-                "u.firstName = :firstName, " +
-                "u.email = :email, " +
-                "u.phoneNumber = :phoneNumber, " +
-                "u.street = :street, " +
-                "u.number = :number, " +
-                "u.postalCode = :postalCode, " +
-                "u.birthDate = :birthDate, " +
-                "u.dateUpdated = :dateUpdated " +
-                "WHERE u.email = :email";
-            session
-                .createMutationQuery(hql)
-                .setParameter("lastName", dto.getLastName())
-                .setParameter("firstName", dto.getFirstName())
-                .setParameter("email", username)
-                .setParameter("phoneNumber", dto.getPhoneNumber())
-                .setParameter("street", dto.getStreet())
-                .setParameter("number", dto.getNumber())
-                .setParameter("postalCode", dto.getPostalCode())
-                .setParameter("birthDate", dto.getBirthDate())
-                .setParameter("dateUpdated", LocalDateTime.now())
-                .executeUpdate();
-            tx.commit();
-            return UserDTO.fromUserUpdateDTO(username, dto);
-        } catch (Exception e) {
-            tx.rollback();
-            throw new RuntimeException(e);
-        } finally {
-            session.close();
-        }
-    }
 }

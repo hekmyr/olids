a6b5f1743cea342420e04c96d7dd8dcc5dfd2090
hekma <contact@hekma.dev>
2025-06-12 01:00:02 +0200

    [api] Fix cors error

--- api/src/main/java/dev/hekmyr/holidays/api/config/SecurityConfig.java
+++ api/src/main/java/dev/hekmyr/holidays/api/config/SecurityConfig.java
@@ -15,15 +15,15 @@ import org.springframework.web.cors.CorsConfiguration;
 @Configuration
 public class SecurityConfig {
 
-    @Value("${cors.allowed-origins}")
-    private List<String> allowedOrigins;
+        @Value("${CLIENT_URL}")
+        private String clientUrl;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.cors(cors ->
             cors.configurationSource(request -> {
                 var config = new CorsConfiguration();
-                config.setAllowedOrigins(allowedOrigins);
+                config.setAllowedOrigins(List.of(clientUrl));
                 config.setAllowedMethods(
                     Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 );
